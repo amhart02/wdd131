ed46211912b3cfe2822a15267be04b9c20c39dec
amhart02 <har20010@byui.edu>
2024-05-04 16:48:07 -0600

    lots of changes

---
+++ mission/mission.js
@@ -0,0 +1,14 @@
+const themeSelector = document.getElementById("themeSelector");
+const body = document.body;
+const logo = document.getElementById("img");
+function changeTheme() {
+  const selectedTheme = themeSelector.value;
+  if (selectedTheme === "dark") {
+    body.classList.add("dark");
+    logo.src = "byui-logo_dark.png";
+  } else {
+    body.classList.remove("dark");
+    logo.src = "byui-logo_blue.webp";
+  }
+}
+themeSelector.addEventListener("click", changeTheme);
