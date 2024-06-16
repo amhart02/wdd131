0000c188b2dd210884ffcd80f5da92e82b590091
amhart02 <har20010@byui.edu>
2024-06-15 21:31:29 -0600

    lots of stuffs

---
+++ register/register.js
@@ -0,0 +1,40 @@
+import { participantTemplate, successTemplate } from './templates.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let participantCount = 1;
+
+  const addParticipantBtn = document.getElementById('add');
+  const form = document.querySelector('form');
+  const summaryElement = document.getElementById('summary');
+
+  function totalFees() {
+      let feeElements = document.querySelectorAll("[id^=fee]");
+      feeElements = [...feeElements];
+      return feeElements.reduce((total, feeElement) => {
+          return total + (parseFloat(feeElement.value) || 0);
+      }, 0);
+  }
+
+  function submitForm(event) {
+      event.preventDefault();
+
+      const totalFee = totalFees();
+      const adultName = document.getElementById('adult_name').value;
+
+      form.style.display = 'none';
+      summaryElement.style.display = 'block';
+      summaryElement.innerHTML = successTemplate({
+          adultName: adultName,
+          participantCount: participantCount,
+          totalFee: totalFee
+      });
+  }
+
+  addParticipantBtn.addEventListener('click', () => {
+      participantCount++;
+      const newParticipantHTML = participantTemplate(participantCount);
+      addParticipantBtn.insertAdjacentHTML('beforebegin', newParticipantHTML);
+  });
+
+  form.addEventListener('submit', submitForm);
+});
