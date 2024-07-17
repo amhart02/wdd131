2aeec8a4bb1d3726b95241bac94b499dfb909a5c
amhart02 <har20010@byui.edu>
2024-07-17 09:28:23 -0600

    made css for trivia page

--- movies/maintrivia.js
+++ movies/maintrivia.js
@@ -2,16 +2,26 @@ import trivia from "./script/trivia.js";
 
 // display movies
 function triviaTemplate(trivia, index) {
-    return `<h1>${trivia.title}</h1>
-            <p>${trivia.question}</p>
-            <input type="radio" value="answer1" name="question${index}">
-            <label for="answer1">${trivia.answer1}</label>
-            <input type="radio" value="answer2" name="question${index}>
-            <label for="answer2">${trivia.answer2}</label>
-            <input type="radio" value="answer3" name="question${index}>
-            <label for="answer3">${trivia.answer3}</label>
-            <input type="radio" value="answer4" name="question${index}>
-            <label for="answer4">${trivia.answer4}</label>
+    return `<h1 class="movie-title-trivia">${trivia.title}</h1>
+            <p class="trivia-question">${trivia.question}</p>
+            <section class="answers">
+            <div class="trivia-option">            
+                <input type="radio" value="answer1" name="question${index}">
+                <label for="answer1" class="trivia-answer">${trivia.answer1}</label>
+            </div>
+            <div class="trivia-option">
+                <input type="radio" value="answer2" name="question${index}">
+                <label for="answer2" class="trivia-answer">${trivia.answer2}</label>
+            </div>
+            <div class="trivia-option">
+                <input type="radio" value="answer3" name="question${index}">
+                <label for="answer3" class="trivia-answer">${trivia.answer3}</label>
+            </div>
+            <div class="trivia-option">
+                <input type="radio" value="answer4" name="question${index}">
+                <label for="answer4" class="trivia-answer">${trivia.answer4}</label>
+            </div>      
+            </section>
             `;
 }
 function renderTrivia(trivia) {
